6d7c266961a8020e062608c56dd6eeb239fac103
Joshua Wong <joshua@josh-wong.net>
2026-05-18 23:12:39 -0400

    remove redundant loading.tsx files
    A single loading.tsx at the [sport] level covers all nested routes.

--- app/[sport]/session/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingContent } from "@/components/sports/loading-content";
-
-export default function Loading() {
-    return (
-        <div className="max-w-4xl mx-auto mb-12 space-y-6 px-4 sm:px-6 lg:px-8">
-            <LoadingContent />
-        </div>
-    );
-}
